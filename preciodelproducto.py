e75fcbedef74cad10a6e5430cbe73f328ad5b6f6
Garou-cosmic <richitovar27@gmail.com>
2026-07-31 12:18:52 -0500

    first commit

--- preciodelproducto.py
+++ preciodelproducto.py
@@ -3,7 +3,7 @@ precio = float(input("ingresa el precio del producto:"))
 # pedir el porcentaje del IVA
 iva = float(input("ingresa el porcetaje del iva (ejemplo: 19, sin el simbolo &): "))
 # calcular el valor del IVA 
-valor_iva = precio * iva /100
+valor_iva = precio * iva / 100
 # calcular el precio total 
 total = precio + valor_iva 
 # mostrar el resultado
